1573888b8661e4b2fa7dd5fc7fcb397aface3431
Bwainuna <blakebah2@gmail.com>
2023-09-05 17:13:55 -0600

    Added all my code and updated README

---
+++ Quiz/assets/script.js
@@ -0,0 +1,205 @@
+const quizContainer = document.querySelector(".app");
+const startButton = document.getElementById("start-button");
+
+
+quizContainer.style.display = "none";
+
+startButton.addEventListener("click", () => {
+  startButton.style.display = "none";
+  quizContainer.style.display = "block";
+  startQuiz();
+  startTimer();
+});
+
+const questions = [
+  {
+    question: " Commonly used data types DO NOT include:",
+    answers: [
+      {text: "1. Strings", correct: false},
+      {text: "2. Booleans", correct: false},
+      {text: "3. Numbers", correct: false},
+      {text: "4. Alerts", correct: true},
+    ]
+  },
+  {
+    question: " The condition in an if / else statement is enclosed with _______.",
+    answers: [
+      {text: "1. quotes", correct: false},
+      {text: "2. curly brackets", correct: false},
+      {text: "3. parenthesis", correct: true},
+      {text: "4. square brackets", correct: false},
+    ] 
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ______.",
+    answers: [
+      {text: "numbers and strings", correct: false},
+      {text: "other arrays", correct: false},
+      {text: "booleans", correct: false},
+      {text: "all of the above", correct: true},
+    ]
+  },
+  {
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    answers: [
+      {text: "commas", correct: false},
+      {text: "curly brackets", correct: false},
+      {text: "quotes", correct: true},
+      {text: "parenthesis", correct: false},
+    ] 
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    answers: [
+      {text: "JavaScript", correct: false},
+      {text: "terminal/bash", correct: false},
+      {text: "for loops", correct: false},
+      {text: "console", correct: true},
+    ]
+  }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.querySelector(".answer_button");
+const nextButton = document.querySelector(".next_button");
+const totalQuestions = questions.length;
+
+let currentQuestionIndex = 0;
+let score = 0;
+let timerRunning = false;
+let timer;
+
+function startQuiz(){
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion(){
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("button");
+    answerButton.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener("click", selectAnswer);
+  })
+}
+
+function resetState(){
+  nextButton.style.display = "none";
+  while(answerButton.firstChild){
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
+
+function selectAnswer(e){
+  const selectedButton = e.target;
+  const isCorrect = selectedButton.dataset.correct === "true";
+  if(isCorrect){
+    selectedButton.classList.add("correct");
+    score++;
+  }else{
+    selectedButton.classList.add("incorrect");
+  }
+  Array.from(answerButton.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < questions.length){
+    showQuestion();}
+    else{
+      showScore();
+    }
+  }
+
+
+var timeLeft = 60;
+
+function startTimer() { 
+  if (!timerRunning) {
+  timerRunning = true; // Start the timer
+  const timerEl = document.getElementById("timer-container");
+  timer = setInterval(function () {
+    timerEl.innerHTML = "Time Left: " + timeLeft;
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      showScore();
+    }
+    timeLeft--;
+  }, 1000);
+}
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  }
+});
+
+startQuiz();
+startTimer();
+
+function selectAnswer(e) {
+  if (!timerRunning) {
+    timerRunning = true; // Start the timer if it's not running already
+  }
+
+  const selectedButton = e.target;
+  const isCorrect = selectedButton.dataset.correct === "true";
+
+  if (isCorrect) {
+    selectedButton.classList.add("correct");
+    score += timeLeft; // Increment score based on remaining time
+  } else {
+    selectedButton.classList.add("incorrect");
+    timeLeft -= 5; // Deduct 5 seconds for incorrect answers
+  }
+
+  Array.from(answerButton.children).forEach(button => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+
+  nextButton.style.display = "block";
+  
+}
+
+function showScore() {
+  clearInterval(timer);
+  resetState();
+  const totalTimeGiven = 60;
+  const calculatedScore = timeLeft + 1;
+  questionElement.innerHTML = `You scored ${calculatedScore} out of 50!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+  nextButton.addEventListener("click", () => {
+    location.reload();
+  });
+}
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # 04-Blake-Boiteux-Quiz
 
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+Screenshots of deployed app:
+
+Sources that were used during this assignment came from class in canvas, as well as through the resources listed below:
+
+https://www.w3schools.com/js/js_functions.asp
 How To Make Quiz App Using JavaScript | Build Quiz App With HTML CSS & JavaScript https://youtu.be/PBcqGxrr9g8 by GreatStack
+ChatGPT by OpenAI @ https://openai.com/blog/chatgpt
